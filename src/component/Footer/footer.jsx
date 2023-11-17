44fbf2660bee3ff8a8ca11dfbef29ad176b9408b
svfannn <efanering02@gmail.com>
2023-11-18 00:51:16 +0800

    first launch

--- src/component/Footer/footer.jsx
+++ src/component/Footer/footer.jsx
@@ -12,17 +12,20 @@ const Footer = () => {
           <h4 className="text-uppercase mb-4">Location</h4>
           <p className="lead mb-0">
             BITUNG CITY, NORTH SULAWESI
-            <br />
-            Sefanya ering
+          <h4 className='text-uppercase mb 4'> Contact me</h4>
+          <p className='lead mb-0'>
+            email: sefanyaering@gmail.com <br />
+            phone: 08123456
+          </p>
           </p>
         </div>
         {/* Footer Social Icons*/}
         <div className="col-lg-4 mb-5 mb-lg-0">
-          <h4 className="text-uppercase mb-4">find me</h4>
-          <a className="btn btn-outline-light btn-social mx-1" href="#!"><i className="fab fa-fw fa-facebook-f" /></a>
-          <a className="btn btn-outline-light btn-social mx-1" href="#!"><i className="fab fa-fw fa-twitter" /></a>
-          <a className="btn btn-outline-light btn-social mx-1" href="#!"><i className="fab fa-fw fa-linkedin-in" /></a>
-          <a className="btn btn-outline-light btn-social mx-1" href="#!"><i className="fab fa-fw fa-dribbble" /></a>
+        <h4 className="text-uppercase mb-4">find me</h4>
+  <a className="btn btn-outline-light btn-social mx-1" href="https://www.instagram.com/evfanering_/?utm_source=ig_web_button_share_sheet&igshid=OGQ5ZDc2ODk2ZA==">
+    <i className="fab fa-instagram"></i>
+    <p className="lead mb-0">Instagram</p>
+  </a>
         </div>
       </div>
     </div>
--- src/component/Portofolio Section/Portofolio.jsx
+++ src/component/Portofolio Section/Portofolio.jsx
@@ -22,25 +22,7 @@ const Portofolio = () => {
             <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
               <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x" /></div>
             </div>
-            <img className="img-fluid" src="/img/portfolio/cabin.png" alt="..." />
-          </div>
-        </div>
-        {/* Portfolio Item 2*/}
-        <div className="col-md-6 col-lg-4 mb-5">
-          <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal2">
-            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-              <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x" /></div>
-            </div>
-            <img className="img-fluid" src="/img/portfolio/cake.png" alt="..." />
-          </div>
-        </div>
-        {/* Portfolio Item 3*/}
-        <div className="col-md-6 col-lg-4 mb-5">
-          <div className="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal3">
-            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-              <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x" /></div>
-            </div>
-            <img className="img-fluid" src="/img/portfolio/circus.png" alt="..." />
+            <img className="img-fluid" src="/img/portfolio/Hackerserti.png" alt="..." />
           </div>
         </div>
       </div>
